df2745c11f7331ee3a887e5ead05895800b2c223
nikolak710329 <nikolak710329@gmail.com>
2022-03-16 19:42:40 +0100

    Dodate equals,hashCode i toStrting metode u klase Magacin i Artikal

--- src/magacin/Magacin.java
+++ src/magacin/Magacin.java
@@ -14,6 +14,36 @@ public class Magacin {
 	public void setArtikli(List<Artikal> artikli) {
 		this.artikli = artikli;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((artikli == null) ? 0 : artikli.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Magacin other = (Magacin) obj;
+		if (artikli == null) {
+			if (other.artikli != null)
+				return false;
+		} else if (!artikli.equals(other.artikli))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Magacin [artikli=" + artikli + "]";
+	}
 	
 	
 }
